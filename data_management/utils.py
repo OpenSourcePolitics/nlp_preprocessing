f3b8a9c62f2f6c317689acb059253fc0e144c02f
QuentinLp <quentin.le-port@ensg.eu>
2021-07-06 15:58:18 +0200

    Adding function to run tests before preprocessing computation : check if file exists, file extension etc.

---
+++ data_management/utils.py
@@ -0,0 +1,45 @@
+"""
+Tool function used across the module
+"""
+import os
+import pandas as pd
+
+
+def check_file_extension(file_path):
+    """
+
+    :param file_path:
+    :return:
+    """
+    filename, file_extension = os.path.splitext(file_path)
+    filename = os.path.basename(os.path.normpath(filename))
+    if file_extension == ".xls":
+        dataframe = pd.read_excel(file_path)
+    elif file_extension == ".csv":
+        dataframe = pd.read_csv(file_path, sep=",", encoding="utf-8")
+    return dataframe, filename
+
+
+def check_preprocessed_file_exists(file_path):
+    """
+    This function aims to save computation time if the data already exist
+    :return: boolean if file exist, path to said file and filename
+    :rtype: tuple
+    """
+    filename, _ = os.path.splitext(os.path.split(file_path)[1])
+    preprocessed_file_path = os.path.join(os.getcwd(), "dist/{}_preprocessed.csv".format(filename))
+    file_exists = os.path.isfile(preprocessed_file_path)
+    return file_exists, preprocessed_file_path, filename
+
+
+def check_category_exists(dataframe):
+    """
+    This function will check if a column "category" exists in the initial dataframe.
+    If the data is not categorized then it will create a false category for statistical computation purposes
+    :return:
+    """
+    if "category" in dataframe.columns:
+        pass
+    else:
+        dataframe["category"] = "false_category"
+    return dataframe
