03325786b7cf399170c220a9908443fc0d528280
QuentinLp <quentin.le-port@ensg.eu>
2021-07-16 11:38:45 +0200

    Adding documentation and load_data()

--- data_management/utils.py
+++ data_management/utils.py
@@ -9,9 +9,12 @@ UTILS_PATH = os.path.split(os.path.realpath(__file__))[0]
 
 def check_file_extension(file_path):
     """
-
-    :param file_path:
-    :return:
+    This function test the file extension and use
+    the adequate method to load the data.
+    :param file_path:path to the data
+    :type file_path: str
+    :return: dataframe, filename
+    :rtype: tuple
     """
     filename, file_extension = os.path.splitext(file_path)
     filename = os.path.basename(os.path.normpath(filename))
@@ -25,6 +28,8 @@ def check_file_extension(file_path):
 def check_preprocessed_file_exists(file_path):
     """
     This function aims to save computation time if the data already exist
+    :param file_path: path to the data
+    :type file_path: str
     :return: boolean if file exist, path to said file and filename
     :rtype: tuple
     """
@@ -38,6 +43,7 @@ def check_category_exists(dataframe):
     """
     This function will check if a column "category" exists in the initial dataframe.
     If the data is not categorized then it will create a false category for statistical computation purposes
+    :param dataframe: pandas dataframe object storing the data
     :return: dataframe updated if it did not have a column category
     """
     if "category" in dataframe.columns:
@@ -45,3 +51,16 @@ def check_category_exists(dataframe):
     else:
         dataframe["category"] = "false_category"
     return dataframe
+
+
+def load_data(file_path):
+    """
+    This functions is used to load the data which will be preprocessed. It deals
+    with csv and xls and will create a category if there is none
+    :param file_path: path to the data
+    :return: dataframe, filename
+    :rytpe: tuple
+    """
+    dataframe, filename = check_file_extension(file_path)
+    dataframe = check_category_exists(dataframe)
+    return dataframe, filename
